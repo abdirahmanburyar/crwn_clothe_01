af5750f0fc9440221bb82cbbf87688f2afb68fe1
abdirahman buryar <buryar313@gmail.com>
2020-05-12 12:12:02 -0700

    added redux-persist

--- src/redux/root-reducer/index.js
+++ src/redux/root-reducer/index.js
@@ -1,7 +1,18 @@
 import { combineReducers } from 'redux'
 import userReducer from '../user/userReducer'
 import cartToggling from '../cart/cart.reducer'
-export default combineReducers({
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart', 'user']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartToggling
 })
+
+export default persistReducer(persistConfig, rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
-
+import { persistStore } from 'redux-persist'
 import rootReducer from './root-reducer'
 import { composeWithDevTools } from 'redux-devtools-extension';
 const middlewares = [logger]
@@ -12,5 +12,8 @@ const store = createStore(
         )
     )
 
-
-export default store
+const persistor = persistStore(store)
+export { 
+    store,
+    persistor
+}
