882f5da39ed51d2dce59860af39c9959b7652f4e
abdirahman buryar <buryar313@gmail.com>
2020-05-14 11:06:58 -0700

    to heroku

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -8,15 +8,20 @@ import { selectCollectionsForPriview } from '../../redux/shop/shop.reselector';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => {
+  console.log(collections)
+  return (
   <div className='collections-overview'>
     {
-      collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))
+      collections.map(item => {
+        console.log(item)
+        return (<CollectionPreview key={item.id} item={item} />)
+      }
+      )
     }
   </div>
-);
+  )
+  };
 
 const mapStateToProps = createStructuredSelector({
   collections: selectCollectionsForPriview
